a64ce89bd03ddac6ef5d19cf9abcfa3fa2d3920b
unknown <kostyaostrom@gmail.com>
2025-11-28 17:42:09 +0300

    Change getting info from unite to separate by fbs/fbo

--- acceptance_acts/acceptance_acts_new.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from act_utils import main
-
-if __name__ == "__main__":
-    asyncio.run(main())
---
+++ acceptance_acts/acceptance_fbo_acts.py
@@ -0,0 +1,6 @@
+import asyncio
+from utils_act import main_fbo
+
+
+if __name__ == "__main__":
+    asyncio.run(main_fbo())
